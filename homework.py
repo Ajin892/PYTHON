d5a133ae1d1503a9b498d9c57d915a3f098553af
Ajin892 <ajinzcbg@gmail.com>
2026-09-07 18:57:05 +0530

    Add initial implementation of price calculation and random number generation

---
+++ homework.py
@@ -0,0 +1,18 @@
+import random
+rice=45
+sugar=40
+oil=130
+riceqty = 3.0; 
+sugarqty = 2.5; 
+oilqty = 1.8;
+ricetotal = rice*riceqty
+sugartotal=sugar*sugarqty
+oiltotal=oil*oilqty
+print ("Rice Total Price",ricetotal)
+print("Sugar Total Price",sugartotal)
+print("Oil Total Price",oiltotal)
+totalprice=int(ricetotal+sugartotal+oiltotal)
+print("Total Price",totalprice)
+totalprice=str(totalprice)
+print("Total Price",totalprice)
+print(random.randrange(5, 10))
