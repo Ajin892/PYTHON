1a973f6fbd29d98b43bfa09189e99982949074d0
Ajin892 <ajinzcbg@gmail.com>
2026-09-08 19:31:56 +0530

    Add initial implementation of string analysis and formatting

---
+++ homework_2.py
@@ -0,0 +1,24 @@
+cha = """Learn Python programming from
+basics to advanced concepts
+through practical projects"""
+length = len(cha)
+first = cha[0]
+last = cha[-1]
+sli = cha[:50]
+replace1 = cha.replace("Python", "PYTHON")
+chac = replace1.lower()
+a = chac.strip()
+list1 = a.split()
+print(length)
+print(first)
+print(last)
+print(sli)
+print(a)
+print(list1)
+b = "course" in a
+if b:
+    print("course in paragraph")
+print("The course description is {} characters long and has {} words.".format(
+    length, len(list1)
+))
+
